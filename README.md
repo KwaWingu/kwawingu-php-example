7a13188606ae7e1fc0f5ad9a9d30a29ed6948a46
Matt Quinn <matt@mattjquinn.com>
2022-04-24 14:15:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# KwaWingu PHP example
+
+Run locally using your own Apache webserver with PHP extensions installed, or on KwaWingu.com in a PHP webserver container.
+
+
---
+++ index.php
@@ -0,0 +1,2 @@
+<p>Hello from <a href="https://github.com/KwaWingu/kwawingu-php-example">https://github.com/KwaWingu/kwawingu-php-example</a>!</p>
+<?php echo phpinfo(); ?>
